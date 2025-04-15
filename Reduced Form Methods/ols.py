7e8da7150f3aefc6a0df146d8e45c7599b3f15f0
Aaron Banks <atbanks@mit.edu>
2025-04-15 10:56:51 -0400

    slight refactoring

--- Reduced Form Methods/ols.py
+++ Reduced Form Methods/ols.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import numpy as np
 import statsmodels.formula.api as smf
 import jax.numpy as jnp 
 import chex 
@@ -17,7 +18,9 @@ class Setting:
     Research question: What is the effect of an agent's
     level of education on their wages? 
     """
-    num_obs: int
+    num_obs: int                # number of observations
+    num_loc: int                # number of cities in the data
+    loc_ind: jnp.ndarray        # unique locational values
     # parameter
     beta: jnp.ndarray           # true parameter
     # outcome
@@ -30,16 +33,26 @@ class Setting:
     ability: jnp.ndarray        # measure of skill falling between 0 and 1 (1 is most)
     location: jnp.ndarray       # location variable denoted by index
 
-
-def DGP(num_obs:int=100, seed:int=17)->Setting:
+def DGP(num_obs:int=100, misspefication:bool=False, seed:int=17)->Setting:
     """
     Data generating process that returns a Setting in
     accordance with the Gauss-Markov assumptions
+
+    Args:
+        num_obs (int, optional): number of observations
+        misspecification (bool, optional): set true model to linear or non-linear, 
+                                           defaults to linear
+        seed (int, optonal): seed for PRNG
+
+    Returns:
+        Setting: realization of the data generating process 
     """
+
     # PRNG keys
     key = jax.random.key(seed)
     subkey1, subkey2, subkey3, subkey4, subkey5, subkey6 = jax.random.split(key, 6)
     
+    _beta = [50, 0.5, 0.6, 0.1, -0.3, 0.4, 0.2]
     _cities = jnp.array([
         0,  # Chicago
         1,  # Boston
@@ -48,8 +61,6 @@ def DGP(num_obs:int=100, seed:int=17)->Setting:
         4,  # Seattle
         5   # Austin 
         ])
-
-    _beta = [50, 0.5, 0.6, 0.1, -0.3, 0.4, 0.2]
 
     # generate covariates
     education = jax.random.randint(subkey1, shape=(num_obs,), minval=1, maxval=12)
@@ -72,13 +83,41 @@ def DGP(num_obs:int=100, seed:int=17)->Setting:
         + epsilon
     )
     _settings_dict = dict(
-        num_obs=num_obs, beta=_beta,wages=wages, education=education, experience=experience,
-        gender=gender, bads=bads, ability=ability, location=location)
+        num_obs=num_obs, num_loc=_cities.size, loc_ind=_cities, beta=_beta,wages=wages, education=education,
+        experience=experience, gender=gender, bads=bads, ability=ability, location=location)
     
     return Setting(**_settings_dict) 
 
+def module_OLS(data:Setting)->jnp.ndarray:
+    """Estimate OLS with statmodels module"""
+
+    # convert jax arrays to pandas object for compatibility
+    # use np.array() to explicity detach jax object from jax coputational graph
+    df = pd.DataFrame({
+            'wages': np.array(data.wages),
+            'education': np.array(data.education),
+            'experience': np.array(data.experience),
+            'gender': np.array(data.gender),
+            'bads': np.array(data.bads),
+            'ability': np.array(data.ability),
+            'location': np.array(data.location)
+        })
+
+    # Specify the regression formula
+    formula = "wages ~ education + experience + gender + bads + ability + C(location)"
+
+    # Fit the model using statsmodels
+    model = smf.ols(formula=formula, data=df).fit()
+    model.summary()
+
+    return
+
 def matrix_OLS(data:Setting)->jnp.ndarray:
     """Estimate OLS via matrix formulation"""
+
+    # transforms locations into fixed effects
+    onehot_loc = (data.location[:, None] == data.loc_ind).astype(int)
+    onehot_loc = onehot_loc[:, 1:] 
 
     # form data matrix
     constant = jnp.ones(shape=(data.num_obs, 1))
@@ -89,14 +128,28 @@ def matrix_OLS(data:Setting)->jnp.ndarray:
         data.gender, 
         data.bads, 
         data.ability,
-        data.location
+        onehot_loc
     ))
     y = data.wages.reshape(-1, 1)
-    
-    # predict parameter \hat{\beta} = (X^T X)^-1 X^T y
-    beta_hat = jnp.linalg.inv(X.T @ X) @ X.T @ y
-    
-    return beta_hat
 
-controls = DGP(100000)
-matrix_OLS(controls)
+    #FIXME multicolinearity somewhere -- X is not inveritble
+    _beta_hat = jnp.linalg.inv(X.T @ X) @ X.T @ y
+
+    return 
+
+def foc_OLS(data:Setting)->jnp.ndarray:
+    """Estimate OLS as the BLUE"""
+    return
+
+def robust_SE()->jnp.ndarray:
+    """Eicker-Huber-White Standard Errors
+    
+    Start here:
+        https://en.wikipedia.org/wiki/Heteroskedasticity-consistent_standard_errors
+    """
+    return  
+
+
+# --- TESTS --- #
+data = DGP(10)
+matrix_OLS(data)
